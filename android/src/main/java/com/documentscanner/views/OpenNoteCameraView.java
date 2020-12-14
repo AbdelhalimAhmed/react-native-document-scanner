56f0215e34bd7b4e89d5059aed23d779dbf39538
Abdelhalim Ahmed <abdelhalim.ahmed95@gmail.com>
2020-12-14 11:07:35 +0200

    make manual capture on android

--- android/src/main/java/com/documentscanner/views/OpenNoteCameraView.java
+++ android/src/main/java/com/documentscanner/views/OpenNoteCameraView.java
@@ -460,9 +460,9 @@ public class OpenNoteCameraView extends JavaCameraView implements PictureCallbac
 
             if (!manualCapture) {
                 sendImageProcessorMessage("previewFrame", new PreviewFrame(mat, true, false));
-            }
-
-        }
+           } else {
+                 sendImageProcessorMessage("previewFrame1", new PreviewFrame(mat, true, false));
+           }
 
     }
 
